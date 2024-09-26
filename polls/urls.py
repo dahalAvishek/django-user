9a85d124dd2738fe27a75631dddd13334c4799b1
Avishek Dahal <dahalavishek2022@gmail.com>
2024-09-26 19:44:44 +0545

    feat: polls app

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<intLquestion_id>/", views.detail, name="detail"),
+    path("<intLquestion_id>/results/", views.results, name="result"),
+    path("<intLquestion_id>/vote/", views.vote, name="vote"),
+    
+    
+    
 ]
